56584b6d73faee8b8e858d77a381ad3a80e42883
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-09-14 14:34:53 +0000

    init android

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,37 +6,50 @@
       :todos="todos"
       :meta="meta"
     ></example-component>
+
+    <div>
+      <div ref="player" />
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { Todo, Meta } from 'components/models';
 import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const todos = ref<Todo[]>([
   {
     id: 1,
-    content: 'ct1'
+    content: 'ct1',
   },
   {
     id: 2,
-    content: 'ct2'
+    content: 'ct2',
   },
   {
     id: 3,
-    content: 'ct3'
+    content: 'ct3',
   },
   {
     id: 4,
-    content: 'ct4'
+    content: 'ct4',
   },
   {
     id: 5,
-    content: 'ct5'
-  }
+    content: 'ct5',
+  },
 ]);
 const meta = ref<Meta>({
-  totalCount: 1200
+  totalCount: 1200,
+});
+
+const player = ref<HTMLDivElement>();
+import jwplayer from 'jwplayer8';
+
+onMounted(() => {
+  jwplayer(player.value).setup({
+    file: 'https://storage.googleapiscdn.com/playlist/62e02a6f2b71af406651c4a0/playlist.m3u8',
+  });
 });
 </script>
