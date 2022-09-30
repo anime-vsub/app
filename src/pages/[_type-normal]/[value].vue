a8b3ca21bf40b0da96bb3390630e1439870d2b05
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-09-30 11:34:40 +0700

    complete section

--- src/pages/[_type-normal]/[value].vue
+++ src/pages/[_type-normal]/[value].vue
@@ -1,72 +1,113 @@
 <template>
   <q-header class="bg-dark-page">
-    <q-toolbar class="relative">
-      <q-btn flat dense round class="mr-2" @click.stop="router.back()">
-        <Icon icon="fluent:chevron-left-24-regular" width="25" height="25" />
-      </q-btn>
+    <q-toolbar v-if="!data" class="relative">
+      <q-skeleton type="QBtn" flat dense round />
 
       <q-space />
-
-      <q-toolbar-title
+      <q-skeleton
+        type="text"
         class="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] text-[16px]"
-        >{{ title }}</q-toolbar-title
-      >
+        width="120px"
+      />
 
-      <q-btn flat dense round @click="showDialogSorter = true">
-        <Icon icon="fluent:filter-24-regular" width="24" height="24" />
-      </q-btn>
+      <q-skeleton type="QBtn" flat dense round />
     </q-toolbar>
 
-    <div class="row text-subtitle1 text-[16px] py-4">
-      <div class="col-4 px-1">
-        <div
-          class="bg-dark-700 px-4 text-zinc-400 py-1 h-[45px] flex items-center justify-between"
-          @click="showDialogGener = true"
-        >
-          Thể loại
-          <Icon icon="fluent:chevron-down-24-regular" />
-        </div>
-      </div>
-      <div class="col-4 px-1">
-        <div
-          class="bg-dark-700 px-4 text-zinc-400 py-1 h-[45px] flex items-center justify-between"
-          @click="showDialogSeaser = true"
-        >
-          Mùa
-          <Icon icon="fluent:chevron-down-24-regular" />
-        </div>
-      </div>
-      <div class="col-4 px-1">
-        <div
-          class="bg-dark-700 px-4 text-zinc-400 py-1 h-[45px] flex items-center justify-between"
-          @click="showDialogSorter = true"
-        >
-          Sắp xếp
-          <Icon icon="fluent:chevron-down-24-regular" />
-        </div>
-      </div>
-    </div>
+    <template v-else>
+      <q-toolbar class="relative">
+        <q-btn flat dense round class="mr-2" @click.stop="router.back()">
+          <Icon icon="fluent:chevron-left-24-regular" width="25" height="25" />
+        </q-btn>
 
-    <div v-if="textFilter" class="text-center text-gray-500 mb-1">
-      Đã chọn:
-      {{ textFilter }}
-    </div>
+        <q-space />
+
+        <q-toolbar-title
+          class="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] text-[16px]"
+          >{{ title }}</q-toolbar-title
+        >
+
+        <q-btn flat dense round @click="showDialogSorter = true">
+          <Icon icon="fluent:filter-24-regular" width="24" height="24" />
+        </q-btn>
+      </q-toolbar>
+
+      <div class="row text-subtitle1 text-[16px] py-4">
+        <div class="col-4 px-1">
+          <div
+            class="bg-dark-700 px-4 text-zinc-400 py-1 h-[45px] flex items-center justify-between"
+            @click="showDialogGener = true"
+          >
+            Thể loại
+            <Icon icon="fluent:chevron-down-24-regular" />
+          </div>
+        </div>
+        <div v-if="defaultsOptions.typer" class="col-4 px-1">
+          <div
+            class="bg-dark-700 px-4 text-zinc-400 py-1 h-[45px] flex items-center justify-between"
+            @click="showDialogSeaser = true"
+          >
+            Mùa
+            <Icon icon="fluent:chevron-down-24-regular" />
+          </div>
+        </div>
+        <div v-else class="col-4 px-1">
+          <div
+            class="bg-dark-700 px-4 text-zinc-400 py-1 h-[45px] flex items-center justify-between"
+            @click="showDialogTyper = true"
+          >
+            Loại
+            <Icon icon="fluent:chevron-down-24-regular" />
+          </div>
+        </div>
+        <div class="col-4 px-1">
+          <div
+            class="bg-dark-700 px-4 text-zinc-400 py-1 h-[45px] flex items-center justify-between"
+            @click="showDialogSorter = true"
+          >
+            Sắp xếp
+            <Icon icon="fluent:chevron-down-24-regular" />
+          </div>
+        </div>
+      </div>
+
+      <div v-if="textFilter" class="text-center text-gray-500 mb-1">
+        Đã chọn:
+        {{ textFilter }}
+      </div>
+    </template>
   </q-header>
 
   <q-page>
     <!-- main -->
 
-    <SkeletonGridCard v-if="!data" :count="12" />
+    <div v-if="data?.items.length === 0" class="text-center py-20">
+      <img
+        src="src/assets/monkey.png"
+        width="186"
+        height="174"
+        class="mx-auto"
+      />
 
-    <q-infinite-scroll v-else @load="onLoad" :offset="250">
-      <GridCard :items="data.items" />
+      <div class="text-subtitle1 mt-1">
+        Không tìm thấy gì cả.
+        <br />
+        Thứ này đã biến mất và chúng tôi không thể tìm thấy
+      </div>
+    </div>
 
-      <template v-slot:loading>
-        <div class="row justify-center q-my-md">
-          <q-spinner class="c--main" size="40px" />
-        </div>
-      </template>
-    </q-infinite-scroll>
+    <template v-else>
+      <SkeletonGridCard v-if="loading || !data" :count="12" />
+
+      <q-infinite-scroll v-else @load="onLoad" :offset="250">
+        <GridCard :items="data.items" />
+
+        <template v-slot:loading>
+          <div class="row justify-center q-my-md">
+            <q-spinner class="c--main" size="40px" />
+          </div>
+        </template>
+      </q-infinite-scroll>
+    </template>
   </q-page>
 
   <!-- gener -->
@@ -96,9 +137,12 @@
             no-caps
             flat
             class="py-2 bg-[#292929] text-weight-normal w-full ease-bg"
+            :disable="defaultsOptions.gener === item.value"
             @click="toggleGenres(item)"
             :class="{
-              'bg-main': indexInGenres(item) > -1,
+              'bg-main':
+                defaultsOptions.gener === item.value ||
+                indexInGenres(item) > -1,
             }"
             >{{ item.text }}</q-btn
           >
@@ -137,9 +181,54 @@
             no-caps
             flat
             class="py-2 bg-[#292929] text-weight-normal w-full ease-bg"
+            :disable="!!defaultsOptions.seaser"
             @click="seaser = item.value"
             :class="{
-              'bg-main': seaser === item.value,
+              'bg-main':
+                defaultsOptions.seaser === item.value || seaser === item.value,
+            }"
+            >{{ item.text }}</q-btn
+          >
+        </div>
+        <!-- -->
+      </q-card-section>
+
+      <q-btn flat no-caps class="w-full py-2 mb-2" v-close-popup>Hủy</q-btn>
+    </q-card>
+  </q-dialog>
+
+  <!-- typer -->
+  <q-dialog v-model="showDialogTyper" position="bottom" full-width>
+    <q-card flat class="w-full pt-3">
+      <q-card-section class="py-0 flex items-center justify-between">
+        <div class="text-subtitle1 mx-1">Loại</div>
+
+        <q-btn
+          dense
+          flat
+          no-caps
+          class="text-weight-normal"
+          @click="typer = null"
+          >Đặt lại</q-btn
+        >
+      </q-card-section>
+
+      <q-card-section class="row">
+        <div
+          v-for="item in data?.filter.typer"
+          :key="item.value"
+          class="col-4 col-sm-3 col-md-4 px-1 py-1"
+        >
+          <q-btn
+            dense
+            no-caps
+            flat
+            class="py-2 bg-[#292929] text-weight-normal w-full ease-bg"
+            :disable="!!defaultsOptions.typer"
+            @click="typer = item.value"
+            :class="{
+              'bg-main':
+                defaultsOptions.typer === item.value || typer === item.value,
             }"
             >{{ item.text }}</q-btn
           >
@@ -178,9 +267,11 @@
             no-caps
             flat
             class="py-2 bg-[#292929] text-weight-normal w-full ease-bg"
+            :disable="!!defaultsOptions.sorter"
             @click="sorter = item.value"
             :class="{
-              'bg-main': sorter === item.value,
+              'bg-main':
+                defaultsOptions.sorter === item.value || sorter === item.value,
             }"
             >{{ item.text }}</q-btn
           >
@@ -249,9 +340,12 @@
               no-caps
               flat
               class="py-2 bg-[#292929] text-weight-normal w-full min-h-10 ease-bg"
+              :disable="defaultsOptions.gener === item.value"
               @click="toggleGenres(item)"
               :class="{
-                'bg-main': indexInGenres(item) > -1,
+                'bg-main':
+                  defaultsOptions.gener === item.value ||
+                  indexInGenres(item) > -1,
               }"
               >{{ item.text }}</q-btn
             >
@@ -289,9 +383,11 @@
               no-caps
               flat
               class="py-2 bg-[#292929] text-weight-normal w-full ease-bg"
+              :disable="!!defaultsOptions.typer"
               @click="typer = item.value"
               :class="{
-                'bg-main': typer === item.value,
+                'bg-main':
+                  defaultsOptions.typer === item.value || typer === item.value,
               }"
               >{{ item.text }}</q-btn
             >
@@ -314,9 +410,12 @@
               no-caps
               flat
               class="py-2 bg-[#292929] text-weight-normal w-full ease-bg"
+              :disable="!!defaultsOptions.seaser"
               @click="seaser = item.value"
               :class="{
-                'bg-main': seaser === item.value,
+                'bg-main':
+                  defaultsOptions.seaser === item.value ||
+                  seaser === item.value,
               }"
               >{{ item.text }}</q-btn
             >
@@ -339,9 +438,11 @@
               no-caps
               flat
               class="py-2 bg-[#292929] text-weight-normal w-full ease-bg"
+              :disable="!!defaultsOptions.year"
               @click="year = item.value"
               :class="{
-                'bg-main': year === item.value,
+                'bg-main':
+                  defaultsOptions.year === item.value || year === item.value,
               }"
               >{{ item.text }}</q-btn
             >
@@ -370,6 +471,7 @@ const router = useRouter()
 
 const showDialogGener = ref(false)
 const showDialogSeaser = ref(false)
+const showDialogTyper = ref(false)
 const showDialogSorter = ref(false)
 const showDialogFilter = ref(false)
 
@@ -394,6 +496,30 @@ const textFilter = computed(() => {
       .filter(Boolean)
       .join(" • ")
   )
+})
+
+const defaultsOptions = computed(() => {
+  const { type_normal, value } = route.params
+
+  // ":type_normal(quoc-gia|tag)/:value",
+  // [":type_normal(season)/:value+"
+  switch (type_normal) {
+    case "danh-sach":
+      return { typer: value }
+    case "the-loai":
+      return { gener: value }
+    case "season": {
+      const [season, year] = value
+
+      if (year) {
+        return { seaser: season, year }
+      }
+
+      return { seaser: season }
+    }
+    default:
+      return {}
+  }
 })
 
 const showFullGener = ref(false)
@@ -434,18 +560,20 @@ function fetchTypeNormalValue(page: number, onlyItems: boolean) {
       typer: typer.value,
       year: year.value,
     },
-    route.params
+    defaultsOptions.value
   )
 }
 
-const { data, error, run } = useRequest(() => fetchTypeNormalValue(1, false))
+const { data, error, run, loading } = useRequest(() =>
+  fetchTypeNormalValue(1, false)
+)
 const title = ref("")
 const watcherData = watch(data, (data) => {
   title.value = data.title
   watcherData()
 })
 watch(
-  [genres, seaser, sorter, typer, year],
+  [genres, seaser, sorter, typer, year, defaultsOptions],
   () => {
     console.log("try refresh")
     run()
