a74234116bf3b283615f0feeae2b7f8223dc5c88
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-10-11 09:20:32 +0700

    split

---
+++ src/components/ScreenError.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="h-full flex items-center">
+    <div class="text-center w-full">
+      <img src="~assets/ic_22_cry.png" width="240" class="mx-auto" />
+      <br />
+      <q-btn
+        dense
+        no-caps
+        outline
+        class="px-2"
+        @click="emit('click:retry')"
+        style="color: #00be06"
+        >Thử lại</q-btn
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const emit = defineEmits<{
+  (name: "click:retry"): void
+}>()
+</script>
---
+++ src/components/ScreenNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="h-full flex items-center">
+    <div class="text-center w-full">
+      <img
+        src="~assets/img_tips_error_not_foud.png"
+        width="186"
+        height="174"
+        class="mx-auto"
+      />
+
+      <div class="text-subtitle1 mt-1">Không tìm thấy gì cả.</div>
+    </div>
+  </div>
+</template>
