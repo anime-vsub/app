b6a9e718a68b8e7da71302c2034186d4fc17c0ea
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-10-15 07:03:41 +0700

    cache state search

--- src/apis/runs/pre-search.ts
+++ src/apis/runs/pre-search.ts
@@ -6,7 +6,7 @@ import type PreSearchParser from "../parser/pre-search"
 import { PostWorker } from "../wrap-worker"
 
 export async function PreSearch(query: string) {
-  return await useCache("/", async () => {
+  return await useCache(`/ajax/suggest?q=${query}`, async () => {
     const { data: html } = await post("/ajax/suggest", {
       ajaxSearch: "1",
       keysearch: query,
