accbfdd5e223275b76e0b903d9f2d4e225e5c5a1
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-10-16 14:09:09 +0700

    use spinner loader

--- src/components/ScreenLoading.vue
+++ src/components/ScreenLoading.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="h-full w-full flex items-center">
-    <LaodingAnim />
+  <div class="h-full w-full flex items-center justify-center">
+    <!-- <LaodingAnim /> -->
+        <q-spinner
+            color="main"
+            size="3em"
+          />
   </div>
 </template>
 
