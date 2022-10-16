fd666276c465d5229ca07bb9fe46dbbcbd183dfd
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-10-16 18:13:39 +0700

    fix https://github.com/anime-vsub/app/issues/5

--- windi.config.ts
+++ windi.config.ts
@@ -5,6 +5,7 @@ import scrollSnap from "windicss/plugin/scroll-snap"
 
 export default defineConfig({
   plugins: [lineClamp, scrollSnap, forms],
+  safelist: "children:!px-0"
 })
 
 // !bg-[rgba(0,194,52,.15)]
