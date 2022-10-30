83ce2ad269a50e4781d52e0c6806353d998a72d8
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-10-30 14:02:49 +0700

    add link

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -501,7 +501,7 @@
           Đăng nhập
         </q-btn>
 
-        <q-btn flat no-caps class="font-weight-normal">
+        <q-btn flat no-caps class="font-weight-normal" href="https://anime-vsub.github.io" target="_blank">
           <Icon icon="fluent:phone-24-regular" width="20" height="20" />
           App
         </q-btn>
