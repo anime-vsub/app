4e244bd71247a8d493d6b3b547937c090e5c3bcc
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-11-04 21:10:54 +0700

    fix menu not working fullscreen

--- quasar.config.js
+++ quasar.config.js
@@ -139,7 +139,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: ["Notify", "Dialog", "Loading", "LoadingBar"],
+      plugins: ["AppFullscreen", "Notify", "Dialog", "Loading", "LoadingBar"],
     },
 
     // animations: 'all', // --- includes all animations
