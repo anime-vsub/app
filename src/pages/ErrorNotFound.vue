8dde64e9433f59bf0c37a6060513345ef192cca2
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2022-11-15 20:31:09 +0700

    notallow page not found fullscreen

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
+    class="text-white text-center q-pa-md flex flex-center"
   >
     <div>
       <div style="font-size: 30vh">404</div>
