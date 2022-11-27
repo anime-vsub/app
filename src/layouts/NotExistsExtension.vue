7a434280ea8431b0023669d12994889c5a7ff1f3
Tachibana Shin <3k2gnfjh@duck.com>
2022-11-27 02:52:17 +0000

    add link install extension `AnimeVsub Helper`

--- src/layouts/NotExistsExtension.vue
+++ src/layouts/NotExistsExtension.vue
@@ -8,11 +8,21 @@
         class="airflow-icon mx-auto"
       />
 
-      <h3 class="text-[24px] mt-10">{{ t('can-cai-dat-extension-tro-giup') }}</h3>
+      <h3 class="text-[24px] mt-10">
+        {{ t("can-cai-dat-extension-tro-giup") }}
+      </h3>
       <p class="text-[14px] mt-3 text-gray-400 leading-normal">
-        {{ t('ung-dung-nay-khong-the-hoat-dong-ma-khong-co-extension-animevsub-helper') }}
+        {{
+          t(
+            "ung-dung-nay-khong-the-hoat-dong-ma-khong-co-extension-animevsub-helper"
+          )
+        }}
         <br />
-        {{ t('extension-animevsub-helper-la-cau-noi-quan-trong-de-ung-dung-gui-cac-yeu-cau-toi-may-chu') }}
+        {{
+          t(
+            "extension-animevsub-helper-la-cau-noi-quan-trong-de-ung-dung-gui-cac-yeu-cau-toi-may-chu"
+          )
+        }}
         <br />
         <q-btn
           no-caps
@@ -21,10 +31,33 @@
           rounded
           class="mt-4"
           target="_blank"
-          href="https://github.com/anime-vsub/extension-animevsub-helper"
-          >{{ t('cai-dat-extension-animevsub-helper') }}</q-btn
+          :href="currentBrowser.href"
         >
+          <Icon
+            :icon="currentBrowser.icon"
+            width="35"
+            height="35"
+            class="mr-1 my-1"
+          />
+          {{ t("cai-dat-extension-animevsub-helper") }}
+          {{ t("cho") }} {{ currentBrowser.text }}
+        </q-btn>
       </p>
+
+      <div class="mt-5 children:px-3">
+        <q-btn
+          rounded
+          stack
+          no-caps
+          v-for="{ icon, text } in browsers"
+          :key="text"
+        >
+          <Icon :icon="icon" width="55" height="55" />
+          <span class="mt-[5px] text-white text-[16px] text-weight-regular">{{
+            text
+          }}</span>
+        </q-btn>
+      </div>
       <!-- <img src="~assets/ic_question_exit.png" width="278" /> -->
     </div>
   </div>
@@ -33,14 +66,15 @@
 <script lang="ts" setup>
 import { Icon } from "@iconify/vue"
 import { useHead } from "@vueuse/head"
+import UAParser from "ua-parser-js"
 import { computed } from "vue"
 import { useI18n } from "vue-i18n"
 
 const { t } = useI18n()
 
-const title = computed(() => t('loi-can-cai-dat-extension-tro-giup'))
-const description =
- computed(() =>  t('ung-dung-nay-can-cai-dat-extension-tro-giup-de-hoat-dong-binh-thuong')
+const title = computed(() => t("loi-can-cai-dat-extension-tro-giup"))
+const description = computed(() =>
+  t("ung-dung-nay-can-cai-dat-extension-tro-giup-de-hoat-dong-binh-thuong")
 )
 useHead(
   computed(() => ({
@@ -60,6 +94,40 @@ useHead(
     ],
   }))
 )
+
+const browsers = [
+  {
+    icon: "logos:chrome",
+    text: "Chrome",
+    name: "Chrome",
+    href: "https://github.com/anime-vsub/extension-animevsub-helper/blob/main/install-on-chrome.md",
+  },
+  {
+    icon: "logos:firefox",
+    text: "Firefox",
+    name: "Firefox",
+    href: "https://addons.mozilla.org/vi/firefox/addon/animevsub-helper/",
+  },
+  {
+    icon: "logos:opera",
+    text: "Opera",
+    name: "Opera",
+    href: "https://github.com/anime-vsub/extension-animevsub-helper/blob/main/install-on-chrome.md",
+  },
+  {
+    icon: "logos:microsoft-edge",
+    text: "Edge",
+    name: "Edge",
+    href: "https://microsoftedge.microsoft.com/addons/detail/endghpbficnpbadbdalhbpecpgdcojig",
+  },
+]
+const currentBrowser = computed(() => {
+  const currentBrowserName = new UAParser().getBrowser().name
+
+  return (
+    browsers.find((item) => item.name === currentBrowserName) ?? browsers[0]
+  )
+})
 </script>
 
 <style lang="scss" scoped>
