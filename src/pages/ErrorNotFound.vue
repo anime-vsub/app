70100ed136dc53ef4adce888f9e6d5c7bdba4478
Tachibana Shin <tachibshin@duck.com>
2022-12-29 03:32:18 +0000

    fix ui

--- src/pages/ErrorNotFound.vue
+++ src/pages/ErrorNotFound.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center"
-  >
+  <div class="text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">404</div>
 
