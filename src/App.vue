41550fd20bb99c200018bb21fd8dc69607c7a15a
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2023-01-27 16:43:32 +0700

    ci

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
 </template>
 
 <script setup lang="ts">
-if (process.env.MODE !== "spa") {
-  import('eruda2').then(eruda2 => eruda2.default.init())
-}
-
 import MainLayout from "layouts/MainLayout.vue"
+
+if (process.env.MODE !== "spa") {
+  import("eruda2")
+    .then((eruda2) => eruda2.default.init())
+    .catch(() => console.warn("load eruda2 failure"))
+}
 </script>
