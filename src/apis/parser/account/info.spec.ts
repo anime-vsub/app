cd863112a804bd5e44402ac51a8f80da8fb19007
Tachibana Shin <tachibshin@duck.com>
2023-01-27 11:32:42 +0000

    update test

--- src/apis/parser/account/info.spec.ts
+++ src/apis/parser/account/info.spec.ts
@@ -1,4 +1,5 @@
 import html from "src/__test__/apis/data/account/info.txt?raw"
+import { C_URL } from "src/constants"
 import { describe, expect, test } from "vitest"
 
 import AccountInfo from "./info"
@@ -6,7 +7,9 @@ import AccountInfo from "./info"
 describe("info", () => {
   test("normal", () => {
     expect(AccountInfo(html)).toEqual({
-      avatar: "http://cdn.animevietsub.cc/data/avatar/user-35738.jpg",
+      avatar: `http://cdn.${C_URL.slice(
+        C_URL.indexOf("://") + 3
+      )}/data/avatar/user-35738.jpg`,
       name: "しん",
       email: "oppoepgergh@gmail.com",
       username: "tachib.shin",
