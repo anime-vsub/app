3a7493c72b55fac7449c6dba964ac77c3f4fae9a
Tachibana Shin <45375496+tachibana-shin@users.noreply.github.com>
2023-02-16 14:50:15 +0700

    https://github.com/anime-vsub/desktop-web/commit/6cd4afefeb139f33b812a700d96302fd74b7ec52

--- src/logic/forceHttp2.ts
+++ src/logic/forceHttp2.ts
@@ -1,4 +1,11 @@
+import { redirectOldDomainCDN } from "./redirectOldDomainCDN"
+
+/**
+ * @description: this function has been information through new domain
+ */
 export function forceHttp2(url: string): string {
+  url = redirectOldDomainCDN(url)
+
   if (url.startsWith("http://")) return "https" + url.slice(4)
 
   return url
