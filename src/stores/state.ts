015bfdc2b5b2cc1342bc227f6671eb990e801d07
Tachibana Shin <tachibshin@duck.com>
2023-04-18 13:53:15 +0700

    50 add a button that temporarily disables automatic restoration of viewing sessions only current tab (#51)
    * add button temporarily disable restore watching
    
    * localize
    
    * lint
    
    * fix https://github.com/anime-vsub/app/runs/12821994977
    
    ---------
    
    Co-authored-by: Tachibana Shin <118260404+tachib-shin@users.noreply.github.com>

---
+++ src/stores/state.ts
@@ -0,0 +1,9 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useStateStorageStore =
+defineStore("state-storage", () => {
+  const disableAutoRestoration = ref(false)
+
+  return { disableAutoRestoration }
+})
