7212aa8c1155617dea5952a8ec1800222afa4202
Tachibana Shin <tachibshin@duck.com>
2023-04-23 06:36:24 +0000

    cp store

--- src/stores/state.ts
+++ src/stores/state.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia"
 import { ref } from "vue"
 
 export const useStateStorageStore = defineStore("state-storage", () => {
-  const disableAutoRestoration = ref(false)
+  const disableAutoRestoration = ref<0 | 1 | 2>(0)
 
   return { disableAutoRestoration }
 })
