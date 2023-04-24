2e69be71f2ce168160911836d51fea205122839e
Tachibana Shin <tachibshin@duck.com>
2023-04-24 20:57:31 +0700

    back to last ep if enter to route ep empty (#55)
    * back to last ep if enter to route ep empty
    
    * check hash
    
    * lint

--- src/constants.ts
+++ src/constants.ts
@@ -31,6 +31,8 @@ export const playbackRates = [
 
 export const DELAY_SAVE_VIEWING_PROGRESS = 20_000 // x4 6s
 
+export const TIMEOUT_GET_LAST_EP_VIEWING_IN_STORE = 5_000 // 5s
+
 export const REGEXP_OLD_HOST_CURL = /animevietsub\.(?:\w+)/i
 
 export const HOST_CURL = [
