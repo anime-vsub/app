daf9e1d5235cd9f95604fb84fe2db0eb30b3af0a
Tachibana Shin <tachibshin@duck.com>
2023-05-08 05:12:49 +0700

    fix warn vue-router

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -155,8 +155,8 @@ const routes: RouteRecordRaw[] = [
 
   {
     name: "watch-anime",
-    path: "/phim/:season/:chapName(.+)-:chap(\\d+)", // [feature or defect]
-    alias: ["/phim/:season/:chapName(\\0)?:chap(\\d+)?", "/phim/:season"],
+    path: "/phim/:season/:chapName(.+)?-:chap(\\d+)", // [feature or defect]
+    alias: ["/phim/:season/:chapName(\\0)?:chap(\\d+)", "/phim/:season"],
     component: () => import("pages/phim/_season.vue"),
     meta: {
       hideDrawer: true,
