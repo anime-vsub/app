dc4bb0119921b9c46dfe82f97c97faef00931439
Tachibana Shin <tachibshin@duck.com>
2023-06-12 08:44:05 +0000

    typo

--- src-pwa/custom-service-worker.ts
+++ src-pwa/custom-service-worker.ts
@@ -1,3 +1,4 @@
+/// <reference types="@types/serviceworker" />
 /*
  * This file (which will be your service worker)
  * is picked up by the build system ONLY if
--- src-pwa/pwa-env.d.ts
+++ src-pwa/pwa-env.d.ts
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 declare namespace NodeJS {
   interface ProcessEnv {
     SERVICE_WORKER_FILE: string
