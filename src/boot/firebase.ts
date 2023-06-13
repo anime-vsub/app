5d35d8b58b3ec13882f9b47d4a1c79a0cdb40e0b
Tachibana Shin <tachibshin@duck.com>
2023-06-13 05:16:44 +0000

    (beta): add index for firebase indexeddb

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -1,12 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { getAnalytics } from "@firebase/analytics"
 import { initializeApp } from "@firebase/app"
+import type {Index
+} from "@firebase/firestore";
 import {
   CACHE_SIZE_UNLIMITED,
   initializeFirestore,
   persistentLocalCache,
   persistentMultipleTabManager,
+  setIndexConfiguration
 } from "@firebase/firestore"
+import configure from "app/firebase/firestore.indexes.json"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,6 +34,17 @@ const db = initializeFirestore(app, {
     cacheSizeBytes: CACHE_SIZE_UNLIMITED,
     tabManager: persistentMultipleTabManager(),
   }),
+})
+
+setIndexConfiguration(db, configure as {
+  indexes: Index[]
+})
+// eslint-disable-next-line promise/always-return
+.then(() => {
+  console.log("[Install indexes]: Installed indexes")
+})
+.catch((err) => {
+  console.error("[Install indexes]: failure ", err)
 })
 
 export { app, analytics, db }
