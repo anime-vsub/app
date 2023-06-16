39c7735c1da453e8dea63dff2a3e8e968b2da24c
Tachibana Shin <tachibshin@duck.com>
2023-06-16 09:24:12 +0700

    skip header get

--- src/logic/http.ts
+++ src/logic/http.ts
@@ -26,25 +26,7 @@ async function httpGet(
       ? url
       : {
           url: url.includes("://") ? url : C_URL + url + "#animevsub-vsub",
-          headers: {
-            accept:
-              "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            // "accept-encoding": "deflate, br",F
-            "accept-language": "vi-VN,vi;q=0.9,en;q=0.8,ja;q=0.7",
-            "cache-control": "max-age=0",
-            dnt: "1",
-            "sec-ch-ua":
-              '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "Windows",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "none",
-            "sec-fetch-user": "?1",
-            "sec-gpc": "1",
-            "upgrade-insecure-requests": "1",
-            ...headers,
-          },
+          headers,
         }
   ).then((response) => {
     if (response.status === 403 || response.status === 520) {
