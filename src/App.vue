0527a5c289472122cd28dd0874e1898d68f7be11
Tachibana Shin <tachibshin@duck.com>
2023-08-09 11:42:27 +0700

    Re support native (#80)
    * re-support request
    
    * fix check for update
    
    * devtools
    
    * drm
    
    * lint
    
    * ?
    
    * update readme.md
    
    * typo
    
    * lint

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 import MainLayout from "layouts/MainLayout.vue"
 import { isNative } from "src/constants"
 
-if (import.meta.env.DEV && !isNative) {
+if (import.meta.env.DEV && isNative) {
   import("eruda2")
     .then((eruda2) => eruda2.default.init())
     .catch(() => console.warn("load eruda2 failure"))
