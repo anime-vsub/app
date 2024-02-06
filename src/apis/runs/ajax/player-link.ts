5948215b01e40de3457a840177b73e016794ad04
Tachibana Shin <tachibshin@duck.com>
2024-02-07 00:02:55 +0700

    feat: use native api req player url

--- src/apis/runs/ajax/player-link.ts
+++ src/apis/runs/ajax/player-link.ts
@@ -1,5 +1,6 @@
 import { getQualityByLabel } from "src/logic/get-quality-by-label"
 import { post } from "src/logic/http"
+import { C_URL } from "src/constants"
 
 const addProtocolUrl = (file: string) =>
   file.startsWith("http") ? file : `https:${file}`
@@ -31,41 +32,48 @@ interface PlayerLinkReturn {
   }[]
   readonly playTech: "api" | "trailer"
 }
+
+type Writeable<T> = {
+  -readonly [P in keyof T]: T[P] extends object ? Writeable<T[P]> : T[P]
+}
+
 export function PlayerLink(config: {
   id: string
   play: string
   hash: string
 }): Promise<PlayerLinkReturn> {
   const { id, play, hash: link } = config
-  return post("/ajax/player?v=2019a", {
-    id,
-    play,
-    link,
-    backuplinks: "1"
-  }).then(({ data }) => {
-    if (!data) throw new Error("unknown_error")
-    type Writeable<T> = {
-      -readonly [P in keyof T]: T[P] extends object ? Writeable<T[P]> : T[P]
-    }
-    const config = JSON.parse(data) as Writeable<PlayerLinkReturn>
-    config.link.forEach((item) => {
-      item.file = addProtocolUrl(item.file)
-      switch (
-        (item.label as typeof item.label | undefined)?.toUpperCase() as
-          | Uppercase<Exclude<typeof item.label, undefined>>
-          | undefined
-      ) {
-        case "HD":
-          if (item.preload) item.label = "FHD|HD"
-          break
-        case undefined:
-          item.label = "HD"
-          break
-      }
-      item.qualityCode = getQualityByLabel(item.label)
-      item.type ??= "mp4"
-    })
-
-    return config
+  //  "#animevsub-vsub" + "_extra"
+  // return post("/ajax/player?v=2019a", {
+  //   id,
+  //   play,
+  //   link,
+  //   backuplinks: "1"
+  // }).then(({ data }) => {
+  return fetch(`${C_URL}/ajax/player?v=2019a#animevsub-vsub_extra`, {
+    method: "post",
+    body: new URLSearchParams({ id, play, link, backuplinks: "1" })
   })
+    .then((res) => res.json() as Promise<Writeable<PlayerLinkReturn>>)
+    .then((config) => {
+      config.link.forEach((item) => {
+        item.file = addProtocolUrl(item.file)
+        switch (
+          (item.label as typeof item.label | undefined)?.toUpperCase() as
+            | Uppercase<Exclude<typeof item.label, undefined>>
+            | undefined
+        ) {
+          case "HD":
+            if (item.preload) item.label = "FHD|HD"
+            break
+          case undefined:
+            item.label = "HD"
+            break
+        }
+        item.qualityCode = getQualityByLabel(item.label)
+        item.type ??= "mp4"
+      })
+
+      return config
+    })
 }
