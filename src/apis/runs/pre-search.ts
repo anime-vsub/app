9b6620683249979cc7957412cd991352802d93bf
Tachibana Shin <tachibshin@duck.com>
2024-02-14 14:31:49 +0000

    feat: convert pre-search to fetch API

--- src/apis/runs/pre-search.ts
+++ src/apis/runs/pre-search.ts
@@ -1,16 +1,25 @@
 import { useCache } from "src/apis/useCache"
 import Worker from "src/apis/workers/pre-search?worker"
-import { post } from "src/logic/http"
+import { C_URL } from "src/constants"
+// import { post } from "src/logic/http"
 
 import type PreSearchParser from "../parser/pre-search"
 import { PostWorker } from "../wrap-worker"
 
 export async function PreSearch(query: string) {
   return await useCache(`/ajax/suggest?q=${query}`, async () => {
-    const { data: html } = await post("/ajax/suggest", {
-      ajaxSearch: "1",
-      keysearch: query
-    })
+    // const { data: html } = await post("/ajax/suggest", {
+    //   ajaxSearch: "1",
+    //   keysearch: query
+    // })
+
+    const html = await fetch(`${C_URL}/ajax/suggest#animevsub-vsub_extra`, {
+      method: "post",
+      body: new URLSearchParams({
+        ajaxSearch: "1",
+        keysearch: query
+      })
+    }).then((res) => res.text())
 
     return PostWorker<typeof PreSearchParser>(Worker, html)
   })
