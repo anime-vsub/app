1473b9ee88522cae2a5944ccba5b4438af1f6f50
Tachibana Shin <tachibshin@duck.com>
2024-03-03 07:13:06 +0700

    feat: fix category not found

--- src/apis/parser/[type_normal]/[value].ts
+++ src/apis/parser/[type_normal]/[value].ts
@@ -1,130 +1,65 @@
-import { getInfoTPost } from "../__helpers__/getInfoTPost"
-import { parserDom } from "../__helpers__/parserDom"
+import type TypeNormalValueParser from "src/apis/parser/[type_normal]/[value]"
+import { useCache } from "src/apis/useCache"
+import Worker from "src/apis/workers/[type_normal]/[value]?worker"
+import { PostWorker } from "src/apis/wrap-worker"
+import { get } from "src/logic/http"
 
-export default function TypeNormalValue(
-  html: string,
-  now: number,
-  onlyItems: boolean
+export async function TypeNormalValue(
+  type: string,
+  value: string | string[],
+  page = 1,
+  onlyItems: boolean,
+  options?: {
+    genres: string[]
+    seaser: string | null
+    sorter: string | null
+    typer: string | null
+    year: string | null
+  },
+  defaultsOptions: {
+    genres?: string
+    seaser?: string
+    typer?: string
+    year?: string
+  } = {}
 ) {
-  const $ = parserDom(html)
+  const isCustom =
+    options &&
+    (options.genres.length > 1 ||
+      options.seaser ||
+      options.typer ||
+      options.year ||
+      /* exclude */ type === "danh-sach")
 
-  const items = $(".MovieList:eq(0)")
-    .find(".TPostMv")
-    .map((_i, item) => getInfoTPost($(item), now))
-    .toArray()
-  const curPage = parseInt(
-    $(".current").attr("data") ?? $(".current").attr("title") ?? "1"
-  )
-  const maxPage = parseInt(
-    $(".larger:last-child, .wp-pagenavi > *:last-child").attr("data") ??
-      $(".larger:last-child, .wp-pagenavi > *:last-child").attr("title") ??
-      "1"
-  )
+  const url = isCustom
+    ? [
+        "/danh-sach",
+        options.typer ?? defaultsOptions.typer ?? "all",
+        options.genres
+          .concat(
+            defaultsOptions.typer && defaultsOptions.typer !== "all"
+              ? [defaultsOptions.typer]
+              : []
+          )
+          .join("-") || "all",
+        options.seaser ?? defaultsOptions.seaser ?? "all",
+        options.year ?? defaultsOptions.year ?? "all",
+        `/trang-${page}.html`
+      ].join("/") + (options.sorter ? "?sort=" + options.sorter : "")
+    : `/${type}/${
+        Array.isArray(value) ? value.join("/") : value
+      }/trang-${page}.html` + (options?.sorter ? "?sort=" + options.sorter : "")
 
-  if (onlyItems) {
-    return { items, curPage, maxPage }
-  }
+  return await useCache(url, async () => {
+    const { data: html } = await get(url)
 
-  const title = $(".breadcrumb:eq(0) > li")
-    .slice(1)
-    .map((_i, item) => $(item).text().trim())
-    .toArray()
-    .join(" ")
-    .replace(/:/g, "")
-    .replace(/^Danh sách /i, "")
+    const now = Date.now()
 
-  const filter = {
-    sorter: $("#filter")
-      .find(".fc-main-list > li > a")
-      .map((_i, anchor) => {
-        const text = $(anchor).text()
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const value = new URL(
-          $(anchor).attr("href") ?? "",
-          "https://example.com"
-        ).searchParams.get("sort")!
-        return { text, value }
-      })
-      .toArray(),
-    typer: $("#filter")
-      .find(".fc-filmtype label")
-      .map((_i, item) => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const value = $(item).find("input").attr("value")!
-
-        // eslint-disable-next-line array-callback-return
-        if (value === "all") return
-
-        const text = $(item).text()
-
-        return {
-          text,
-          value,
-        }
-      })
-      .toArray()
-      .filter(Boolean),
-    seaser: $("#filter")
-      .find(".fc-quality label")
-      .map((_i, item) => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const value = $(item).find("input").attr("value")!
-
-        // eslint-disable-next-line array-callback-return
-        if (value === "all") return
-
-        const text = $(item).text()
-
-        return {
-          text,
-          value,
-        }
-      })
-      .toArray()
-      .filter(Boolean),
-    gener: $("#filter")
-      .find(".fc-genre label")
-      .map((_i, item) => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const value = $(item).find("input").attr("value")!
-
-        // eslint-disable-next-line array-callback-return
-        if (value === "all") return
-
-        const text = $(item).text()
-
-        return {
-          text,
-          value,
-        }
-      })
-      .toArray()
-      .filter(Boolean),
-    year: $("#filter")
-      .find(".fc-release label")
-      .map((_i, item) => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const value = $(item).find("input").attr("value")!
-
-        // eslint-disable-next-line array-callback-return
-        if (value === "all") return
-
-        const text = $(item).text()
-
-        return {
-          text,
-          value,
-        }
-      })
-      .toArray()
-      .filter(Boolean),
-  }
-
-  return {
-    title,
-    items,
-    curPage,
-    maxPage,
-    filter,
-  }
+    return PostWorker<typeof TypeNormalValueParser>(
+      Worker,
+      html,
+      now,
+      onlyItems
+    )
+  })
 }
