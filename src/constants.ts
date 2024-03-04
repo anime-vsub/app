d59b850aed41b3bf6f0ea600030fb4be9cfd68ec
Tachibana Shin <tachibshin@duck.com>
2024-03-04 14:56:54 +0000

    WIP: support skip op / end use API 9animetv

--- src/constants.ts
+++ src/constants.ts
@@ -53,3 +53,7 @@ export const C_URL =
   [104, 116, 116, 112, 115, 58, 47, 47]
     .map((val) => String.fromCharCode(val))
     .join("") + HOST_CURL
+
+    
+export const API_OPEND = "https://opend-9animetv.animevsub.eu.org"
+export const WARN = console.warn.bind(console)
