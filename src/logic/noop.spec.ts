ba813764c56f54ffe7c599dfcf79fd276d93d65f
Tachibana Shin <tachibshin@duck.com>
2024-03-23 08:34:50 +0000

    feat: use `unplugin-auto-import` and `unplugin-vue-components`

---
+++ src/logic/noop.spec.ts
@@ -0,0 +1,5 @@
+describe("noop", () => {
+  test("1 + 1 = 2", () => {
+    expect(1 + 1).toBe(2)
+  })
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from "vitest/config"
+import AutoImport from "unplugin-auto-import/vite"
+import IconsResolver from "unplugin-icons/resolver"
+import Icons from "unplugin-icons/vite"
+import Components from "unplugin-vue-components/vite"
 
 export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
   },
+  plugins: [
+    Icons(),
+    Components({
+      resolvers: [
+        IconsResolver({
+          prefix: "i",
+        }),
+      ],
+    }),
+    AutoImport({
+      imports: [
+        "vue",
+        "vue-router",
+        { quasar: ["useQuasar"], "vue-i18n": ["useI18n"] },
+      ],
+      dts: "./auto-imports.d.ts",
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+  ],
 })
