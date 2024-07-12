c150e68019440b809590a0b071fe44c1f8b93a44
Tachibana Shin <tachibshin@duck.com>
2024-07-12 10:43:00 +0700

    chore: change host url

--- src/constants.ts
+++ src/constants.ts
@@ -45,7 +45,7 @@ export const TIMEOUT_GET_LAST_EP_VIEWING_IN_STORE = 5_000 // 5s
 export const REGEXP_OLD_HOST_CURL = /animevietsub\.(?:\w+)/i
 
 export const HOST_CURL = [
-  97, 110, 105, 109, 101, 118, 105, 101, 116, 115, 117, 98, 46, 100, 101, 118
+  97, 110, 105, 109, 101, 118, 105, 101, 116, 115, 117, 98, 46, 97, 112, 112
 ]
   .map((val) => String.fromCharCode(val))
   .join("")
