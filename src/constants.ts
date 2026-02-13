eac2e601f1e48352acfd9272e9170d048e837942
Tachibana Shin <tachibshin@duck.com>
2026-02-13 11:11:07 +0000

    [script]: Update `HOST_CURL`

--- src/constants.ts
+++ src/constants.ts
@@ -46,11 +46,11 @@ export const REGEXP_OLD_HOST_CURL = /animevietsub\.(?:\w+)/i
 
 // @host
 export const HOST_CURL = [
-  97, 110, 105, 109, 101, 118, 105, 101, 116, 115, 117, 98, 46, 118, 105, 112
+  98, 105, 116, 46, 108, 121, 47, 97, 110, 105, 109, 101, 118, 105, 101, 116, 115, 117, 98, 116, 118
 ]
   .map((val) => String.fromCharCode(val))
   .join("")
-// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end
+// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end// @end
 export const C_URL =
   [104, 116, 116, 112, 115, 58, 47, 47]
     .map((val) => String.fromCharCode(val))
